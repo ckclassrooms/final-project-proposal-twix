6852396b5df86661fcdbd4147ab9e22f6f6284d0
Jeet <jmrockstat06@gmail.com>
2022-11-07 11:44:56 -0600

    Oauth-features

--- frontend/bikelane-app/src/components/Navbar.jsx
+++ frontend/bikelane-app/src/components/Navbar.jsx
@@ -1,18 +1,45 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-function Nav() {
-    return (
-        <ul className="nav nav-pills navbar-expand">
-            <li className="nav-item"><NavLink className={({ isActive }) => "nav-link " + (isActive ? " active" : "")}
-                to="/" end>Home</NavLink></li>
-            <li className="nav-item"><NavLink className={({ isActive }) => "nav-link " + (isActive ? " active" : "")}
-                to="/liveDb">Live Database</NavLink></li>
-            <li className="nav-item"><NavLink className={({ isActive }) => "nav-link " + (isActive ? " active" : "")}
-                to="/maps">Maps</NavLink></li>
-            <li className="nav-item"><NavLink className={({ isActive }) => "nav-link " + (isActive ? " active" : "")}
-                to="/submitObstruction">Submit Obstruction</NavLink></li>
-        </ul>
-    )
+import {supabase} from '../supabaseClient'
+function Nav({session, setSession}) {
+    
+    const loginSubmit = async ()=>{
+        // Todo - Add logic to login via Github Oauth
+        let { user, error } = await supabase.auth.signInWithOAuth({
+            provider: 'google'
+          })
+        // setSession("Setting Something as session")
+    }
+
+    const logoutSubmit = async ()=>{
+        // Todo - Add logic to logout
+        let { error } = await supabase.auth.signOut()
+        // setSession(null);
+    }
+
+    if (session != null){
+        return ( <ul className="nav nav-pills navbar-expand">
+        <li className="nav-item"><NavLink className={({ isActive }) => "nav-link " + (isActive ? " active" : "")}
+            to="/" end>Home</NavLink></li>
+        <li className="nav-item"><NavLink className={({ isActive }) => "nav-link " + (isActive ? " active" : "")}
+            to="/add">Live Database</NavLink></li>
+        <li className="nav-item"><NavLink className={({ isActive }) => "nav-link " + (isActive ? " active" : "")}
+            to="/list">Maps</NavLink></li>
+        <li className="nav-item"><NavLink className={({ isActive }) => "nav-link " + (isActive ? " active" : "")}
+            to="/chart">Submit Obstruction</NavLink></li>   
+        <li className="nav-item ms-auto"><button className="btn btn-primary m-1" id='logoutSubmit' onClick={()=>logoutSubmit()}>Logout</button></li>       
+    </ul>)
+    }
+    else {
+    return ( 
+    <ul className="nav nav-pills navbar-expand">
+        <li className="nav-item "><NavLink className={({ isActive }) => "nav-link " + (isActive ? " active" : "")}
+            to="/" end>Home</NavLink></li> 
+            <div className="ms-auto" style={{display:"flex"}}>
+               <li className="nav-item ms-auto"><button className="btn btn-primary m-1" id='loginSubmit' onClick={()=>loginSubmit()}>Login</button></li>
+            </div>              
+    </ul>)
+    }
 }
 
 export default Nav
