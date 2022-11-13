143f7549bfcecbcd93c67f5dd81b1388905c60b0
nitishdewan <90693909+nitishdewan@users.noreply.github.com>
2022-11-13 13:56:20 -0600

    updated SubmitObstruction.jsx for warnings

--- src/components/SubmitObstruction.jsx
+++ src/components/SubmitObstruction.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useState} from 'react';
+// import {useState} from 'react';
 // import Avatar from '../components/Avatar';
 import {supabase} from '../supabaseClient';
 function SubmitObstruction() {
