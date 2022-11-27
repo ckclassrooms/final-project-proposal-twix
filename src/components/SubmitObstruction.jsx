3cb4292aca54e8e491f632b8066b7860ca7d52ba
nitishdewan <90693909+nitishdewan@users.noreply.github.com>
2022-11-26 18:50:14 -0600

    geolocation fix

--- src/components/SubmitObstruction.jsx
+++ src/components/SubmitObstruction.jsx
@@ -44,15 +44,29 @@ function SubmitObstruction() {
         }
         
       }
-
-      function getLocation() {
-        if (navigator.geolocation) {
-          var position = navigator.geolocation.getCurrentPosition();
-          var currLatLong = [position.coords.latitude, position.coords.longitude]
-        }
-        // return currLatLong
-        console.log(currLatLong)
+      function getLocation()
+      {
+        const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+      
+      function success(pos) {
+        const crd = pos.coords;
+      
+        console.log('Your current position is:');
+        console.log(`Latitude : {crd.latitude}`);
+        console.log(`Longitude: {crd.longitude}`);
+        console.log(`More or less {crd.accuracy} meters.`);
       }
+      
+      function error(err) {
+        console.warn(`ERROR({err.code}): ${err.message}`);
+      }
+      
+      navigator.geolocation.getCurrentPosition(success, error, options);
+    }
 
       async function uploadDets(jsonObj) {
         console.log("jsonArray");
