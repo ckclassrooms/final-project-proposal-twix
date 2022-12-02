eb5b96e806c51ab5a3636642c529b7e142469f2d
Jeet Mehta <jmrockstat06@gmail.com>
2022-12-02 15:43:24 -0600

    alert removed

--- src/App.jsx
+++ src/App.jsx
@@ -10,33 +10,35 @@ import About from './components/About';
 import { supabase } from './supabaseClient'
 import { useState, useEffect } from "react";
 function App() {
-
+  //console.log("vALUE ",supabase.auth.user());
   const [session, setSession] = useState(null);
-  const [loginFlag, setLoginflag] = useState(0);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  // const [requests, setRequests] = useState([]);
   
   useEffect(() => {
+    console.log("App.jsx use effect, session = ",session)
     supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session);
+      console.log(" get session body session session",session)
+  
+      // alert("Login Successful!!")
+      // setIsLoggedIn(true);
     });
   
+    //console.log("vALUE ",supabase.auth.user());
     supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session);
+      
+      console.log(" auth state changes session",session)
+  
+      
+      // else{
+      //   alert("logged out")
+      // }
+      // alert("Logged out")
     });
-  }, []);
-
-  useEffect(()=>{
-
-    if (session==null){
-      // console.log("I am null")
-    }
-    else{
-      if (loginFlag === 0){
-        alert("Hi, I am logged in")
-        setLoginflag(1)
-      }
-    }
-
-  }, [session, loginFlag])
+  });
 
   return (
       <>
@@ -47,9 +49,16 @@ function App() {
               <Route path='/liveDb' element={< LiveDb />}></Route>
               <Route path='/maps' element={< Maps />}></Route>
               <Route exact path="/submitObstruction" element={session  ? <SubmitObstruction /> : <Navigate to = "/"/> }></Route>
+              {/* <Route exact path="/submitObstruction" render={() => (
+                supabase.auth.user() ? (
+                <Navigate to = "/submitObstruction"/>
+                ) : (
+                  <Landing />
+                )
+              )}/> */}
           </Routes>
       </>  
   );
 }
 
-export default App;
+export default App;
