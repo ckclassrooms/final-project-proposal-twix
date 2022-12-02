e7c71f3a0e9425f2494fbd4bf2eb75e08e3dec8e
sbhargav <sbhargav@godaddy.com>
2022-12-01 19:37:56 -0600

    stupid git

--- src/components/LiveDb.jsx
+++ src/components/LiveDb.jsx
@@ -5,7 +5,7 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Button from 'react-bootstrap/Button';
 import {Form} from 'react-bootstrap';
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import Pagination from "react-bootstrap/Pagination";
 
 function LiveDb() {
