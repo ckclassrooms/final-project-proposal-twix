0b92862fe4b5df52b953489d9cccd3c26d4040de
nitishdewan <90693909+nitishdewan@users.noreply.github.com>
2022-12-03 17:05:48 -0600

    cleaned up code for maps and removed sanitization

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,18 +9,34 @@ function About() {
                 <h3>Github repo: <a href="https://github.com/ckclassrooms/final-project-proposal-twix.git">Twix Team</a></h3>
                 <h3>App deployment: <a href="https://cool-conkies-80a0da.netlify.app/">Twix App</a></h3>
                 <br/>
-                <h2>Objectives:</h2> <br></br> 
+                <h4>Team Members:</h4>
+                <ul>
+                    <li>
+                        Jeet Mehta 
+                    </li>
+                    <li>
+                        Monisha Siddananda Sampath
+                    </li>
+                    <li>
+                        Sharath S Bhargav
+                    </li>
+                    <li>
+                        Nitish Dewan
+                    </li>
+                </ul>
+                <br/>
+                {/* <h2>Objectives:</h2> <br></br> 
                 <ol>
                     <li>Users should be able to login via Email or an SSO.</li>
                     <li>Once logged in, users should be able to submit a violation with required details using the form provided (including location).</li>
                     <li>Users will be able to visualize all the violations submitted for an area and filter data as needed.</li>
                     <li>Users can also visualize the violations on a map.</li>
-                </ol>
-
+                </ol> */}
+                <h2>Project Architecture:</h2>
                 <img src={require("./images/484-bikespy-flow-chart.jpg")} alt = {"Flow Diagram"} />
 
                 <br></br>
-                <h2>Project Summary:</h2><br/>
+                {/* <h2>Project Summary:</h2><br/>
                 <p>The project involves a web application that allows users to report and submit the details of any Bike Lane violation. Violations mainly consist of a vehicle parked in the bike lane. The application also allows the user to submit images along with other details of violation. This data is stored in a database and can be accessed by any registered user using the interface provided. 
                 <br></br>
                 In addition to the live database view, the data can also be visualized in an interactive map. 
@@ -31,8 +47,8 @@ function About() {
                     <li>The location acquired from the device should be securely read and stored.</li>
                     <li>The authentication methods, SSO or User Sign-up, should be implemented securely.</li>
                     <li>All confidential data should be stored in hashed form, and not as plain-text.</li>
-                </ul>
-                <h2>Current Milestone Achieved</h2>
+                </ul> */}
+            {/* <h2>Current Milestone Achieved</h2>
             <p><ol>
                 <li>The front-end interface and basic functionality such as Navigation Bar, Form Control, layout and Access Control is setup.</li>
                 <li>The user can login via GitHub OAuth which is routed via Supabaae.</li>
@@ -49,9 +65,81 @@ function About() {
                     <li>Fix Mapbox conflicts and have the user visualize violations on Map, only for the visible area of map, controlled by corresponding events.</li>
                 </ol>
             </p>
-            <p><b>Note:</b> Few options such as 'Submit Obstruction' and 'Live DB' are accessible from Navigation Bar without login for testing purposes. Final version will have everything in Nav Bar locked under access control.</p>
-            </div>
+            <p><b>Note:</b> Few options such as 'Submit Obstruction' and 'Live DB' are accessible from Navigation Bar without login for testing purposes. Final version will have everything in Nav Bar locked under access control.</p> */}
+
+
+            <h2>Application Functionality:</h2>
+            <p>
+                <ol>
+                    <li>
+                        The user is greeted with an initial welcome page with a Nav Bar (containing About, Live Database, Maps, and Login Buttons).
+                    </li>
+                    <li>
+                        Once the user clicks on login, the user is redirected to a Github Login Page, where the user can authorize our application (Github OAuth set using Supabase)
+                    </li>
+                    <li>
+                        If the user is not logged in, they can navigate to the following pages:
+                        <ul>
+                            <li>
+                            <b>Live Database:</b> Displays a Grid View of all the obstructions reported in a paginated manner. This page also allows filtering the results based on Violation Type, Date of Violation, and City of reported Violation. The grid displays the city of violation, the violation type, and the date and time of the violation report. If the user has uploaded an image, submitted a license plate, and submitted the location, the same will be displayed as well.
+                            </li>
+                            <li>
+                            <b>Maps View:</b> Visualizes the obstructions on a map. The obstructions are loaded based on the visible area of the map, and if the user changes the visible area/zooms in or out, the button 'Search Again' needs to be clicked to refresh the map to reload the violations in this new visible area. Users can click on violation markers to open a popup with details of the violation (including an image if submitted).
+                            </li>
+                            <li>
+                            <b>About Page:</b> This page gives an overall idea of the infrastructure, and design of the Web Application.
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                    Once the user is authenticated, the button to 'Submit Obstruction' is enabled in Nav Bar. Going to this page, a form is presented to submit a new obstruction. Users can submit the following details:
+                    <ul>
+                        <li>
+                            <b>Mandatory</b>
+                            <ul>
+                                <li>Type of Violation</li>
+                                <li>City of Violation</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <b>Optional</b>
+                            <ul>
+                                <li>Licence Plate</li>
+                                <li>Geo-location</li>
+                                <li>Single Image of Violation</li>
+                                <li>Additional Notes</li>
+                            </ul>
+                        </li>
+                    </ul>
+                    </li>
+                </ol>
+            </p>
+            <h2>Beyond the declared scope:</h2>
+            <p>
+                <ol>
+                    <li>
+                        <b>Polygon Select on Map:</b> 
+                    </li>
+                </ol>
+            </p>
+            <h2>Notable Highlights</h2>
+            <p>
+                <ol>
+                    <li>
+                        <b>PostGIS:</b>
+                    </li>
+                </ol>
+            </p>
+            <b>Screen Recording</b> for Map functionality is available at: [drive link]
             
+            <h2>Additional Points to Note:</h2>
+            <p>
+                <ol>
+                    <li>Get Location for Submit Obstruction page might not work on all browsers. This has been tested extensively on Brave and Chrome, where it works for most cases. The issue is randomly occuring.</li>
+                    <li>Polygon, as explined in the video, needs to be deleted exclusively using the delete button before searching again using the filter options.</li>
+                </ol>
+            </p>
+            </div>
         </>
     )
 }
--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,41 +1,29 @@
 import React from 'react'
 import '../scss/mapbox-gl.css'
 import { useRef, useEffect, useState } from 'react';
-
-// eslint-disable-next-line import/no-webpack-loader-syntax
-import mapboxgl from '!mapbox-gl';
 import MapboxDraw from "@mapbox/mapbox-gl-draw";
 import { supabase } from '../supabaseClient';
-import DOMPurify from 'dompurify';
-
 import Button from 'react-bootstrap/Button';
-// import {Form} from 'react-bootstrap';
 import Multiselect from 'multiselect-react-dropdown';
 import { violationTypes } from './Violation';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import mapboxgl from '!mapbox-gl';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
-// mapboxgl.accessToken = "pk.eyJ1Ijoibml0aXNoZGV3YW4iLCJhIjoiY2xhM2ZqcXlzMGFxZjNvbDRkMHFjOHBjYyJ9.d7qTDfI-UTq6QwfUxbsfZw"
-console.log(mapboxgl.accessToken)
 
 const draw = new MapboxDraw({
     displayControlsDefault: false,
-    // Select which mapbox-gl-draw control buttons to add to the map.
     controls: {
         polygon: true,
         trash: true
     },
-    // Set mapbox-gl-draw to draw by default.
-    // The user does not have to click the polygon control button first.
     defaultMode: 'simple_select'
 });
 
 
 
 function Maps() {
-
     var [mapLoaded, setLoadedMap] = useState(false)
-
-    // const [violation, setViolation] = useState('construction');
 
     const mapContainer = useRef();
     // const mapContainer = React.createRef();
@@ -45,9 +33,6 @@ function Maps() {
     const zoom = 11
 
     const [violation, setViolation] = useState([]);
-    // const [lng, setLng] = useState(-87.64);
-    // const [lat, setLat] = useState(41.87);
-    // const [zoom, setZoom] = useState(10);
 
     var layer_exists = false
 
@@ -59,12 +44,14 @@ function Maps() {
             center: [lng, lat],
             zoom: zoom
         });
+
         map.current.loadImage(
             "https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png",
             (error, image) => {
                 if (error) throw error;
                 map.current.addImage('custom-marker', image);
             })
+
         map.current.on('load', () => {
             console.log('test map on load')
             setLoadedMap(true);
@@ -78,17 +65,13 @@ function Maps() {
             const data = draw.getAll();
             if (draw.getMode() === 'draw_polygon') {
                 var pids = []
-
-                // ID of the added template empty feature
                 const lid = data.features[data.features.length - 1].id
-
                 data.features.forEach((f) => {
                     if (f.geometry.type === 'Polygon' && f.id !== lid) {
                         pids.push(f.id)
                     }
                 })
                 draw.delete(pids)
-
             }
         });
     });
@@ -97,7 +80,6 @@ function Maps() {
         if (e.type === 'draw.delete') {
             removeData()
             Map_gen();
-            
         }
         else if (e.type === 'draw.create') {
             removeData()
@@ -105,9 +87,7 @@ function Maps() {
             const locArray = poly.features[0].geometry.coordinates[0];
             loadPolygonData(locArray)
             console.log("Draw .create called", locArray);
-            // layer_exists = true
             console.log("layer_exists" + layer_exists)
-
         }
         else if (e.type === 'draw.update') {
             const poly = draw.getAll();
@@ -125,33 +105,21 @@ function Maps() {
         };
 
         function success(pos) {
-            // eslint-disable-next-line
             const crd = pos.coords;
-
-            console.log('Your current position is:');
-            console.log(`Latitude : ${crd.latitude}`);
-            console.log(`Longitude: ${crd.longitude}`);
-            console.log(`More or less ${crd.accuracy} meters.`);
 
             var currLat = crd.latitude
             var currLong = crd.longitude
-            console.log("hello")
-            console.log(currLat, currLong)
 
             map.current.flyTo({
-                // center: [(Math.random() - 0.5) * 360, (Math.random() - 0.5) * 100],
                 center: [currLong, currLat],
-                essential: true // this animation is considered essential with respect to prefers-reduced-motion
+                essential: true
             });
         }
 
         function error(err) {
             console.warn(`ERROR({err.code}): ${err.message}`);
         }
-
         navigator.geolocation.getCurrentPosition(success, error, options);
-
-        console.log("test 2")
     }
 
     function changeViolationValue(valueArray) {
@@ -162,7 +130,6 @@ function Maps() {
         setViolation(valueArray);
     }
 
-    // eslint-disable-next-line
     async function loadPolygonData(array) {
         removeData()
         var array_cat = []
@@ -171,21 +138,17 @@ function Maps() {
             array_cat.push(checkboxes[i].value)
         }
         let payload = { "poly": array }
-        console.log(violation.length);
         if (violation.length > 0) payload["cats"] = violation
-        console.log("payload of polygon", payload)
+
         const { data, error } = await supabase.functions.invoke('maps_polygon_1', {
             body: payload
         }
         )
-        console.log("polygon res", data)
         if (error)
             console.log("Error", error)
 
         loadMapWithData(data)
         layer_exists = true
-        console.log("test here")
-        console.log(layer_exists)
 
     }
 
@@ -201,11 +164,12 @@ function Maps() {
 
         const { data, error } = await supabase.functions.invoke('maps_func_2', {
             body: payload
+        })
+        if (error != null){
+            alert("Unable to fetch data from server.")
         }
-        )
-        console.log(data)
-        console.log(error)
-
+        
+        
         return data
     }
 
@@ -222,26 +186,14 @@ function Maps() {
         if (map.current.getSource("points_source")) {
             map.current.removeSource("points_source");
         }
-
-        // try{
-        // map.current.removeImage('custom-marker')
-        // }
-        // catch{
-        //     console.log("error in removing image")
-        // }
-
-        // }
     }
 
     async function loadMapWithData(data) {
         removeData()
-
         map.current.addSource('points_source', {
             'type': 'geojson',
             'data': data
         })
-        console.log('source added')
-
 
         map.current.addLayer({
             'id': 'points',
@@ -260,22 +212,13 @@ function Maps() {
             }
         });
 
-
-
         map.current.on('click', 'points', (e) => {
-            // Copy coordinates array.
-
             console.log("inside map click")
             const coordinates = e.features[0].geometry.coordinates.slice();
-            const violation = DOMPurify().sanitize(unescape(e.features[0].properties.violation));
-            const timeViolationDate = DOMPurify().sanitize(unescape((e.features[0].properties.time).slice(0, 10)));
-            console.log(timeViolationDate)
-            const timeViolationTime = DOMPurify().sanitize(unescape((e.features[0].properties.time).slice(11, 19)));
-            console.log(timeViolationTime)
-            const imageURL = DOMPurify().sanitize(unescape(e.features[0].properties.image_url));
-
-            // console.log("typeof")
-            // console.log(typeof(violation))
+            const violation = e.features[0].properties.violation;
+            const timeViolationDate = (e.features[0].properties.time).slice(0, 10);
+            const timeViolationTime = (e.features[0].properties.time).slice(11, 19);
+            const imageURL = e.features[0].properties.image_url;
 
             while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
                 coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
@@ -288,66 +231,31 @@ function Maps() {
                 .addTo(map.current);
         });
         layer_exists = true
-        console.log("layer added")
     }
 
     function Map_gen() {
-        console.log("inisde map gen " + layer_exists)
         removeData()
-        console.log("bounds all")
         const curr_bounds = map.current.getBounds()
-        console.log("ne-lang" + curr_bounds['_ne']['lng'])
-        console.log("ne-lat" + curr_bounds['_ne']['lat'])
-        console.log("sw-lang" + curr_bounds['_sw']['lng'])
-        console.log("se-lang" + curr_bounds['_sw']['lat'])
-        console.log("test button press", mapLoaded)
-        // console.log(document.getElementById('f2').value)
 
         var array_cat = []
         var checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
         for (var i = 0; i < checkboxes.length; i++) {
             array_cat.push(checkboxes[i].value)
         }
-        console.log(array_cat)
 
-
-        // console.log(violation)
         if (mapLoaded === true) {
-
-            console.log("test button")
-
             async function getMap() {
-                console.log('calling edge func')
-
-                // values for testing
                 var lat1 = curr_bounds['_sw']['lng']
                 var lon1 = curr_bounds['_ne']['lat']
                 var lat2 = curr_bounds['_ne']['lng']
                 var lon2 = curr_bounds['_sw']['lat']
-                // var data = await supabaseCall(lat1, lon1, lat2, lon2, array_cat)
+                
                 var data = await supabaseCall(lat1, lon1, lat2, lon2, violation)
                 loadMapWithData(data)
-                // layer_exists = true
-
             }
             (async () => await getMap())()
         }
     }
-
-    // function toggleTrue(source) {
-    //     var checkboxes = document.getElementsByClassName("form-check-input")
-    //     for(var i=0, n=checkboxes.length;i<n;i++) {
-    //       checkboxes[i].checked = true;
-    //     }
-    //   }
-
-    // function toggleFalse(source) {
-    //     var checkboxes = document.getElementsByClassName("form-check-input")
-    //     for(var i=0, n=checkboxes.length;i<n;i++) {
-    //       checkboxes[i].checked = false;
-    //     }
-    // }
-    // })
 
     return (
         <div class="container-xxl" style={{ marginTop: "60px" ,width: "100%;"}}>
@@ -358,13 +266,11 @@ function Maps() {
                 <link href="https://api.mapbox.com/mapbox-gl-js/v2.9.2/mapbox-gl.css" rel="stylesheet" />
                 <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.2.2/mapbox-gl-draw.js"></script>
                 <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.2.2/mapbox-gl-draw.css" type="text/css"></link>
-                {/* <style dangerouslySetInnerHTML={{__html: "\n  body { margin:0; padding:0; }\n  #map { position:absolute; top:50px; bottom:0; width:100%; }\n" }} /> */}
+
                 <div class="col-12">
 
                     <h5>Select the filters you want to apply: <br /> (Default: All results will be displayed)</h5>
-                    {/* <Button style={{t: "30px"}} onClick={() => toggleTrue()}>Select All</Button> */}
-                    {/* &nbsp;&nbsp;&nbsp; */}
-                    {/* <Button onClick={() => toggleFalse()}>De-select All</Button> */}
+        
                     <br />
                     <Multiselect class="form-select"
                         options={violationTypes}
